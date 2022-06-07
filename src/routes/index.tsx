f0a8193310fab988642b314af38f16b5d9725128
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-06-07 16:43:19 -0300

    initial base for a react project

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import Home from '../pages/Home';
+
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+  );
+};
+
+export default AppRoutes;
